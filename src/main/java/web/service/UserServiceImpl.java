78132ae57f78cf3ce00ea28ad324a4f6628b07c2
NataliaFedorova <59181907+NataliaFedorova@users.noreply.github.com>
2021-10-02 17:56:18 +0300

    Update UserServiceImpl.java

--- src/main/java/web/service/UserServiceImpl.java
+++ src/main/java/web/service/UserServiceImpl.java
@@ -37,37 +37,26 @@ public class UserServiceImpl implements UserService{
     @Override
     @Transactional
     public void addUser(User user) {
-        Set<Role> roles = new HashSet<>();
-        if (user.getRoles().contains(new Role("ROLE_ADMIN"))) {
-            roles.add(roleDao.findRoleById(1L));
-        } else if (user.getRoles().contains(new Role("ROLE_USER"))) {
-            roles.add(roleDao.findRoleById(2L));
-        }
-        user.setRoles(roles);
+        getRoles(user);
         userDao.addUser(user);
     }
 
     @Override
     @Transactional
     public void updateUser(Long id, User user) {
-        User userToBeUpdated = userDao.getOneUser(id);
-        userToBeUpdated.setUsername(user.getUsername());
-        userToBeUpdated.setName(user.getName());
-        userToBeUpdated.setSurname(user.getSurname());
-        userToBeUpdated.setEmail(user.getEmail());
-        userToBeUpdated.setAge(user.getAge());
-        //пароль сразу в зашифрованном виде
-        userToBeUpdated.setPassword(user.getPassword());
 
-        Set<Role> roles = new HashSet<>();
+        getRoles(user);
+        userDao.updateUser(id, user);
+    }
 
-        if (user.getRoles().contains(new Role("ROLE_ADMIN"))) {
-            roles.add(roleDao.findRoleById(1L));
+    private void getRoles(User user) {
+        Set<Role> rolesFindByName = new HashSet<>();
+
+        Set<Role> roles = user.getRoles();
+        for (Role role : roles) {
+            rolesFindByName.add(roleDao.findRoleByName(role.getRoleName()));
         }
-        roles.add(roleDao.findRoleById(2L));
-        userToBeUpdated.setRoles(roles);
-
-        userDao.updateUser(id, userToBeUpdated);
+        user.setRoles(rolesFindByName);
     }
 
     @Override
