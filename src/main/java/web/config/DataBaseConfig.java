d98da8ca351fa9b1ac4e78fb35abd5c3c3ce1366
NataliaFedorova <59181907+NataliaFedorova@users.noreply.github.com>
2021-10-02 17:50:31 +0300

    Update DataBaseConfig.java

--- src/main/java/web/config/DataBaseConfig.java
+++ src/main/java/web/config/DataBaseConfig.java
@@ -33,19 +33,91 @@ public class DataBaseConfig {
         this.environment = environment;
     }
 
+    package web.config;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan("web")
+@PropertySource("classpath:app.properties")
+
+public class DataBaseConfig {
+
+    private Environment environment;
+
+    public DataBaseConfig(Environment environment) {
+        this.environment = environment;
+    }
+
     @Bean
-    public DataSource dataSource() {
+    public DataSource dataSource() throws NullPointerException {
         BasicDataSource ds = new BasicDataSource();
-        try {
-            ds.setUrl(environment.getRequiredProperty("app.url"));
-            ds.setDriverClassName(Objects.requireNonNull(environment.getRequiredProperty("app.driver")));
-            ds.setUsername(environment.getRequiredProperty("app.username"));
-            ds.setPassword(environment.getRequiredProperty("app.password"));
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
+        ds.setUrl(environment.getRequiredProperty("app.url"));
+        ds.setDriverClassName(Objects.requireNonNull(environment.getRequiredProperty("app.driver")));
+        ds.setUsername(environment.getRequiredProperty("app.username"));
+        ds.setPassword(environment.getRequiredProperty("app.password"));
         return ds;
     }
+
+    @Bean
+    public PlatformTransactionManager platformTransactionManager() {
+        JpaTransactionManager manager = new JpaTransactionManager();
+        manager.setEntityManagerFactory(entityManagerFactory().getObject());
+        return manager;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        return new HibernateJpaVendorAdapter();
+    }
+
+    @Bean
+    public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+        return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory () {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource());
+        em.setPackagesToScan(environment.getRequiredProperty("app.entity.package"));
+        em.setJpaVendorAdapter(jpaVendorAdapter());
+        em.setJpaProperties(getHibernateProperties());
+        return em;
+    }
+
+    private Properties getHibernateProperties() {
+        try {
+            Properties properties = new Properties();
+            InputStream is = getClass().getClassLoader().getResourceAsStream("hibernate.properties");
+            properties.load(is);
+            return properties;
+        } catch (IOException ex) {
+            throw new IllegalArgumentException("Can't find 'hibernate.properties' in classpath!", ex);
+        }
+    }
+}
+
 
     @Bean
     public PlatformTransactionManager platformTransactionManager() {
